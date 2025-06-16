7b789d702f5ac5f259ff321af31cecbd5c707438
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-16 20:19:31 +0300

    edit page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
+'use client'
 import Goods from "./components/goods";
 import Reviews from "./components/rewievs";
 import ShoppingCart from "./components/shoppingCart";
+import StoreProvider from "./store/storeProvider";
 
 export default function Home() {
+
+  // var store = useAppSelector(state => state.reviews);
+
+  // console.log(store);
   return (
     <div className="page">
       <header className="page__header">
         <h1 className="page__title">тестовое задание</h1>
       </header>
-
-      <Reviews />
-      <ShoppingCart></ShoppingCart>
-      <Goods></Goods>
+      <StoreProvider>
+        <Reviews />
+        <ShoppingCart></ShoppingCart>
+        <Goods></Goods>
+      </StoreProvider>
     </div>
   );
 }
