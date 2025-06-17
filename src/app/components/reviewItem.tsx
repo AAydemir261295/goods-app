94fdadc5007eaa12b8fc772b5d9e967b109ed38c
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-17 12:45:08 +0300

    edit reviewItem.tsx

--- src/app/components/reviewItem.tsx
+++ src/app/components/reviewItem.tsx
@@ -1,6 +1,6 @@
 export default function ReviewItem(props: any) {
     return <li className="reviews-container__reviews-item"
         dangerouslySetInnerHTML={{ __html: props.inner }}
-        key={props.id}>
+        >
     </li>;
 }
