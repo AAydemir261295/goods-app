97c7d32ac2a0f746a4509322318385652caceaf1
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-17 11:51:11 +0300

    edit store.ts

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,26 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reviewsReducer from "./reducers/reviewsReducer";
-import goodsReducer from "./reducers/goodsReducer";
-
-
-// export const store = configureStore({
-//     reducer: {
-//         reviews: reviewsReducer,
-//         goods: goodsReducer
-//     }
-// })
+import { apiSlice } from "./apiSlice";
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
-            reviews: reviewsReducer,
-            goods: goodsReducer
-        }
+            [apiSlice.reducerPath]: apiSlice.reducer,
+        },
+        middleware: getDefaultMiddleware =>
+            getDefaultMiddleware().concat(apiSlice.middleware)
+
     })
 }
 
 export type AppStore = ReturnType<typeof makeStore>
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
-
