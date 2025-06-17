89a31cc590fec3dd6c4e350a69523935baa8b851
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-17 11:51:22 +0300

    edit reviews.tsx

--- src/app/components/rewievs.tsx
+++ src/app/components/rewievs.tsx
@@ -1,9 +1,32 @@
+'use client'
+import { useGetReviewsQuery } from "../store/apiSlice";
+import { ReactNode } from "react";
+import dompurify from 'dompurify';
+import ReviewItem from "./reviewItem";
+
 
 
 export default function Reviews() {
-    
-    
+
+    var items: ReactNode[] = [];
+
+    const {
+        data: reviews = [],
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetReviewsQuery();
+
+
     return <div className="reviews-container">
-                
+        <ul className="reviews-container__reviews nostyle-list">
+            {isLoading ?
+                "loading" :
+                reviews.map((v) =>
+                    <ReviewItem id={v.id} inner={dompurify.sanitize(v.text, { FORCE_BODY: true })}
+                    />)
+            }
+        </ul>
     </div>;
 }
