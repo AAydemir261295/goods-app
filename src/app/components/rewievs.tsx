9e12f971b8cdc2bcc9e0fa56d88bf0f674c235b3
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-17 16:35:17 +0300

    edit reviews.tsx afte store changes

--- src/app/components/rewievs.tsx
+++ src/app/components/rewievs.tsx
@@ -1,28 +1,27 @@
 'use client'
-import { useGetReviewsQuery } from "../store/apiSlice";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import dompurify from 'dompurify';
 import ReviewItem from "./reviewItem";
 import Loader from "./loader";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchReviews } from "../store/thunks";
 
 
 
 export default function Reviews() {
+    var dispatch = useDispatch() as any;
+    var items = useSelector(state => state.reviews.items);
+    var isLoading = useSelector(state => state.reviews.isPending);
 
-    const {
-        data: reviews = [],
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    } = useGetReviewsQuery();
-
+    useEffect(() => {
+        dispatch(fetchReviews());
+    }, [dispatch])
 
     return <div className="reviews-container">
         {isLoading ?
             <Loader></Loader> :
             <ul className="reviews-container__reviews nostyle-list">
-                {reviews.map((v) =>
+                {items.map((v) =>
                     <ReviewItem key={v.id} id={v.id} inner={dompurify.sanitize(v.text, { FORCE_BODY: true })}
                     />)
                 }
