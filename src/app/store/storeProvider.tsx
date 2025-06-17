2b2cf4ace6e9a1565eb28d022e7625fcd3b3f7cd
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-17 11:51:38 +0300

    edit storeProvider.tsx

--- src/app/store/storeProvider.tsx
+++ src/app/store/storeProvider.tsx
@@ -2,8 +2,6 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { AppStore, makeStore } from './store'
-import { retrieveGoods } from './reducers/goodsReducer'
-import { retrieveReviews } from './reducers/reviewsReducer'
 
 export default function StoreProvider({
     children
@@ -13,9 +11,6 @@ export default function StoreProvider({
     const storeRef = useRef<AppStore | null>(null)
     if (!storeRef.current) {
         storeRef.current = makeStore()
-        storeRef.current.dispatch(retrieveGoods({}));
-        storeRef.current.dispatch(retrieveReviews({}));
-
     }
 
     return <Provider store={storeRef.current}> {children} </Provider>
