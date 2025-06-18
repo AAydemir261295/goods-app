248db5bf5984514de1abd378244f6934c3ae77d8
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-18 14:44:33 +0300

    edit page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-import Goods from "./components/goods";
+import Goods  from "./components/goods";
 import Reviews from "./components/rewievs";
 import ShoppingCart from "./components/shoppingCart";
 import StoreProvider from "./store/storeProvider";
@@ -12,7 +11,7 @@ export default function Home() {
         <header className="page__header">
           <h1 className="page__title">тестовое задание</h1>
         </header>
-        {/* <Reviews /> */}
+        <Reviews />
         <ShoppingCart></ShoppingCart>
         <Goods></Goods>
       </div>
