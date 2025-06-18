e4948d5926a5424d0ac904545c9521b1e933966f
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-18 08:37:03 +0300

    edit shoppingCartReducer

--- src/app/store/reducers/shoppingCartReducer.ts
+++ src/app/store/reducers/shoppingCartReducer.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Good } from "../models/Goods";
+import Goods from "@/app/components/goods";
 
 interface InitialState {
     goods: Good[],
@@ -19,7 +20,15 @@ const shoppingCartReducer = createSlice({
             return state = { ...state, goods: { ...action.payload.goods }, telephone: action.payload.telephone };
         },
         updateOrder(state, action: { payload: Good }) {
-            return state = { ...state, goods: state.goods.map((v: Good) => v.id == action.payload.id ? action.payload : v) }
+            var tmp = state.goods.find((v) => v.id == action.payload.id);
+            if (tmp == undefined) {
+                return state = { ...state, goods: [...state.goods, action.payload] };
+            } else {
+                return state = { ...state, goods: state.goods.map((v: Good) => v.id == action.payload.id ? action.payload : v) }
+            }
+        },
+        removeGood(state, action: { payload: number }) {
+            return state = { ...state, goods: state.goods.filter((v) => v.id != action.payload) }
         },
         updatePhoneNumber(state, action: { payload: string }) {
             return state = { ...state, telephone: action.payload }
@@ -30,6 +39,6 @@ const shoppingCartReducer = createSlice({
 
 const { actions, reducer } = shoppingCartReducer;
 
-export const { updateCart, updateOrder, updatePhoneNumber } = actions;
+export const { updateCart, updateOrder, removeGood, updatePhoneNumber } = actions;
 
 export default reducer;
