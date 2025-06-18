ead09e0ed4daea725360de8611d836d7ff22ecd1
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-18 14:46:02 +0300

    edit goodsList.tsx

--- src/app/components/goodsList.tsx
+++ src/app/components/goodsList.tsx
@@ -1,34 +1,52 @@
 'use client'
 import { Good } from "../store/models/Goods";
 import GoodItem from "./goodItem";
+import Loader from "./loader";
+import { retrieveGoods } from "../backendApi/goodsApi";
+import { useState } from "react";
 
 export default function GoodsList(props: any) {
-    var items = props.items;
-    // var dispatch = useDispatch();
-    // var items = useSelector((state: any) => state.goods.items) as any;
-    // var isLoading = useSelector((state: any) => state.goods.isPending);
+    var [items, setItems] = useState(props.items);
+    var [isLoading, setIsLoading] = useState(false);
+    var lastPage = 2;
+    var totalPages = 11
+    var size = 20;
+
+    async function loadContent(e: any) {
+        let scrollableArea = e.target;
+        let maxScroll = scrollableArea.scrollHeight - 1500 - 500;
+        let scrollPosition = scrollableArea.scrollTop;
 
 
-    // function test() {
-    //     console.log(state);
-    // }
 
-    return <ul className="goods-container__goods-list nostyle-list">
+        if (scrollPosition >= maxScroll) {
+            if (!isLoading) {
+                setIsLoading(true);
+                var data = await retrieveGoods(lastPage, size)
+                if (data) {
+                    setIsLoading(false);
+                    setItems([...items, ...data.items]);
+                    if (lastPage < totalPages) {
+                        lastPage++;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    return <><ul className="goods-container__goods-list nostyle-list"
+        onScroll={loadContent}>
         {
-            items.map((v: Good) =>
-                <GoodItem key={v.id} imageSrc={v.image_url} title={v.title} description={v.description} price={v.price} ></GoodItem>
+            items.map((v: Good, idx: number) =>
+                <GoodItem key={idx} id={v.id} imageSrc={v.image_url} title={v.title} description={v.description} price={v.price} ></GoodItem>
             )
         }
 
-        {/* <GoodItem id={1} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={2} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={3} imageSrc={"asdf"} title="название" description={"DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTIONDESCRIPTION DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={4} imageSrc={"asdf"} title="название" description={"DESCRIPTION слово слово слово слово"} price={11111} ></GoodItem>
-            <GoodItem id={5} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={6} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={7} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={8} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem>
-            <GoodItem id={9} imageSrc={"asdf"} title="название" description={"DESCRIPTION"} price={11111} ></GoodItem> */}
-
     </ul>
+        {isLoading ? <Loader /> : ""}/
+
+
+    </>
 }
