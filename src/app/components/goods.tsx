823f1c1b7caad51de95eb813073a478be6f998c0
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-18 14:46:13 +0300

    edit goods.tsx

--- src/app/components/goods.tsx
+++ src/app/components/goods.tsx
@@ -1,8 +1,5 @@
-import Loader from "./loader";
-import GoodItem from "./goodItem";
-import { Good } from "../store/models/Goods";
 import GoodsList from "./goodsList";
-// import { useSelector } from "react-redux";
+
 
 const apiUrl = (page: number, size: number) => `http://o-complex.com:1337/products?page=${page}&page_size=${size}`;
 
@@ -11,10 +8,9 @@ export default async function Goods() {
     const data = await fetch(apiUrl(1, 20))
     var goods = await data.json();
     var items = goods.items;
-    console.log(items);
 
 
     return <div className="goods-container">
-        <GoodsList items={items}></GoodsList>
+            <GoodsList items={items}></GoodsList>
     </div>;
 }
