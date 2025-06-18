0f845e2c4ab00f148fc118378fefe4243b8763d9
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-18 15:30:21 +0300

    edit shoppingCart.tsx

--- src/app/components/shoppingCart.tsx
+++ src/app/components/shoppingCart.tsx
@@ -1,25 +1,33 @@
+'use client'
 import { useSelector } from "react-redux";
 import { Good } from "../store/models/Goods";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TelephoneInput from "./telephoneInput";
+import { ShoppingCartState } from "../store/reducers/shoppingCartReducer";
+import { sendBuyRequest } from "../backendApi/shoppingApi";
 
 
 export default function ShoppingCart() {
-    const defaultValue = '+7 (___) ___ __-__';
+    var cart = useSelector((state: { cart: ShoppingCartState }) => state.cart);
+    var items = useSelector((state: { cart: ShoppingCartState }) => state.cart.goods);
+    var [error, setError] = useState("");
 
-    var items = useSelector((state: any) => state.shoppingCart);
-    var [phoneValue, setPhoneValue] = useState(defaultValue);
-    var [error, showError] = useState("")
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart])
+
 
     function isValidNumber() {
-        return phoneValue.indexOf("_") == -1;
+        return cart.telephone.indexOf("_") == -1;
     }
 
     function buy() {
         if (isValidNumber()) {
+            const body = { phone: cart.telephone, cart: items.map((v: Good) => { return { id: v.id, quantity: v.count } }) }
+            // sendBuyRequest()
 
         } else {
-
+            setError("input--error");
         }
     }
 
@@ -29,13 +37,19 @@ export default function ShoppingCart() {
         <header className="shopping-cart-container__header">
             <h6 className="shopping-cart-container__title">Добавленные товары</h6>
         </header>
-        <ul className=" shopping-cart-container__order-list nostyle-list">
-            {/* {items.goods.map((v: Good) => 
-            <li></li>
-            )} */}
+        <ul className="shopping-cart-container__order-list nostyle-list">
+            {items.map((v: Good, idx: number) =>
+                <li key={idx} className="shopping-cart-container__order-row">
+                    <h3>{v.title}</h3>
+                    <h3>{`x${v.count}`}</h3>
+                    <h3>{`${v.price}₽`}</h3>
+                </li>
+
+
+            )}
         </ul>
         <form action={buy} className="shopping-cart-container__form nostyle-form">
-            <TelephoneInput value={phoneValue} setPhoneValue={setPhoneValue} showError={error}></TelephoneInput>
+            <TelephoneInput showError={error} setErrorState={setError}></TelephoneInput>
             <button className="shopping-cart-container__btn btn nostyle-btn" type="submit">заказать</button>
         </form>
     </div>;
